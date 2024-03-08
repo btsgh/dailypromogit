8c1f3a0c3e8a0ed614f2e09f47058b54d248861b
Aparna Ramachandran <aparna@appsmith.com>
2024-03-08 08:39:31 +0000

    c

--- pages/Page3/jsobjects/JSObject1/JSObject1.js
+++ pages/Page3/jsobjects/JSObject1/JSObject1.js
@@ -1,13 +1,14 @@
 export default {
 	myVar1: [],
 	myVar2: {},
-	myFun1 () {
+	myFun3 () {
+		setInterval()
 		setInterval(() => {
 			showAlert('hi')
 		}, 5000, 'interval1')
 		//	this.myVar1 = [1,2,3]
 	},
-	async myFun2 () {
+	async myFun4 () {
   		clearInterval('interval1')
 		//	await storeValue('varName', 'hello world')
 	}
---
+++ pages/Page3/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+	moment.calendarFormat()
+		_.assign()
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
