baf45d3726a63ce2beb1f837b50cf825ce2943a3
Aparna Ramachandran <aparna@appsmith.com>
2024-05-07 10:30:14 +0000

    commit

--- pages/Page3/jsobjects/JSObject1/JSObject1.js
+++ pages/Page3/jsobjects/JSObject1/JSObject1.js
@@ -1,13 +1,13 @@
 export default {
 	myVar1: [],
 	myVar2: {},
-	myFun1 () {
+	myFun () {
 		setInterval(() => {
 			showAlert('hi')
 		}, 5000, 'interval1')
 		//	this.myVar1 = [1,2,3]
 	},
-	async myFun2 () {
+	async myFun5 () {
   		clearInterval('interval1')
 		//	await storeValue('varName', 'hello world')
 	}
