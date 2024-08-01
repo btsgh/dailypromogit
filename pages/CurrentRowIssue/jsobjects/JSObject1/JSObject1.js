6debc5f0e153689b0e8616492e705f726cd72ace
Aparna Ramachandran <aparna@appsmith.com>
2024-08-01 07:16:20 +0000

    commit with the currentRow issue page added

---
+++ pages/CurrentRowIssue/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,17 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	//dataRow: { id: 1, name: 'Alice', age: 30 },
+	row: {},
+
+	myFun1 (row) {
+		console.log('Row data:', row);
+	}
+	//,
+	//	 updateDataRow() {
+	//      if (Table1 && Table1.selectedRowIndex !== undefined) {
+	//        this.row = { id: Table1.selectedRowIndex }; // Update dataRow with the selected row index
+	//  } else {
+	//    console.error('Table1 or selectedRowIndex is not defined.');
+	// }
+}
---
+++ pages/CurrentRowIssue/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,15 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	
+	{ 
+	newfunction: function(row) 
+		{
+			console.log(row.id);
+		},
+	
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
